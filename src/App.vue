d640384a0945f5bc364a4e6d8d0768df64cd28db
poman <poman1638m@gmail.com>
2020-02-22 16:06:38 +0900

    copy from https://github.com/penpenpng/vue-project-boilerplate

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template lang="pug"></template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style lang="stylus" src="@assets/style/common.stylus"></style>
+<style scoped lang="stylus">
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const Path = require("path")
+
+const { HotModuleReplacementPlugin } = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const VueLoaderPlugin = require("vue-loader/lib/plugin")
+
+const src = Path.resolve(__dirname, "src")
+const dist = Path.resolve(__dirname, "dist")
+
+
+module.exports = {
+    mode: "development",
+    context: src,
+    entry: {
+        main: "./main.ts",
+    },
+    output: {
+        filename: "[name].js",
+        path: dist,
+    },
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.(ts|js|vue)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "eslint-loader",
+                    options: {
+                        fix: true,
+                    },
+                },
+            },
+            {
+                test: /\.vue$/i,
+                use: "vue-loader",
+            },
+            {
+                test: /\.pug$/i,
+                oneOf: [
+                    {
+                        resourceQuery: /^\?vue/,
+                        use: ["pug-plain-loader"]
+                    },
+                    {
+                        use: "pug-loader",
+                    }
+                ]
+            },
+            {
+                test: /\.ts$/i,
+                use: {
+                    loader: "ts-loader",
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/i],
+                    }
+                },
+            },
+            {
+                test: /\.styl(us)?$/i,
+                use: ["vue-style-loader", "css-loader", "stylus-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: "file-loader",
+            },
+        ]
+    },
+    plugins: [
+        new HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./index.pug",
+        }),
+    ],
+    resolve: {
+        extensions: [".js", ".ts", ".vue"],
+        alias: {
+            "@assets": Path.resolve(src, "assets"),
+            "@components": Path.resolve(src, "components"),
+        },
+    },
+    devServer: {
+        contentBase: dist,
+    },
+}
